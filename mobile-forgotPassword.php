fd2131c99a1fc2f0c9c31c84a7897bdabff32b8f
abhishakedixitin <er.abhishakedixitin@gmail.com>
2019-04-07 20:11:58 -0700

    Update mobile-forgotPassword.php

--- mobile-forgotPassword.php
+++ mobile-forgotPassword.php
@@ -186,7 +186,7 @@
               
         <h3 style="font-size: 50%vw; text-align:center">Get link to reset Password</h3><br><br>
       <h4 style="font-size: 15%vw">Registered Email Id:</h4>
-          <input type="email" id="mail" name="user_email" style="border-color:rgb(110,65,30);border-width:thin" size=30vw required><br>
+          <input type="email" id="mail" name="user_email" style="border-color:rgb(110,65,30);border-width:thin" size="" required><br>
                    <p> <a href= "forgotEmail.php" style="color:blue;margin-top:5%; font-size: 110%;text-decoration: underline">Forgot Email Id?</a></p>
           
               <center><button class="button" type="submit" style="margin-top:6%">Submit</button></center>
